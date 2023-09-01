6f260e1e66ae11fdcbffebda4648f636aa06fb4e
Victor Barreto <victor.barreto@MacBook-Pro.local>
2023-09-01 19:04:59 -0300

    aula-4

---
+++ aula-4/Polimorfismo.ts
@@ -0,0 +1,73 @@
+abstract class Forma {
+  abstract calcularArea(): number;
+  getPI() {
+    console.log("obtendo PI");
+    return Math.PI;
+  }
+}
+
+interface Teste {
+  getPi(): number;
+}
+
+class TesteClasse implements Teste {
+  getPi(): number {
+    throw new Error("Method not implemented.");
+  }
+}
+
+class Circulo extends Forma {
+  constructor(public raio: number) {
+    super();
+  }
+  calcularArea(): number {
+    return Math.PI * Math.pow(this.raio, 2);
+  }
+
+  getPI(): number {
+    return 123;
+  }
+}
+
+class Retangulo extends Forma {
+  largura: number;
+  altura: number;
+
+  constructor(largura: number, altura: number) {
+    super();
+    this.altura = altura;
+    this.largura = largura;
+  }
+
+  calcularArea(): number {
+    return this.largura * this.altura;
+  }
+}
+
+function teste() {
+  const forma = 1;
+}
+
+function teste2() {
+  const forma = "123";
+}
+
+function calcularAreaTotal(formas: Forma[]) {
+  let areaTotal = 0;
+
+  for (const forma of formas) {
+    areaTotal += forma.calcularArea();
+  }
+
+  return areaTotal;
+}
+
+const circulo = new Circulo(5);
+
+const retangulo = new Retangulo(3, 4);
+const areaTotal = calcularAreaTotal([circulo, retangulo]);
+
+console.log("Area total:", areaTotal);
+
+console.log("TESTE:", circulo.getPI());
+console.log("TESTE 2:", retangulo.getPI());
