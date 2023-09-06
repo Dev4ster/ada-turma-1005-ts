528a1c22c01011da668c777f01ba9979ccd4f26f
Victor Barreto <victor.barreto@ifood.com.br>
2023-09-06 20:36:13 -0300

    html e css - jogo da velha

--- aula-6/jogo-velha/script.ts
+++ aula-6/jogo-velha/script.ts
@@ -1,6 +1,10 @@
+/**
+ * use esse comando pra buildar
+ * tsc script.ts --outFile script.js --target esnext
+ *  */
 enum Simbolo {
-  X,
-  O,
+  X = "X",
+  O = "O",
 }
 
 class Player {
@@ -35,9 +39,9 @@ class Board {
       const [a, b, c] = possibilidade;
 
       return (
-        this.celulas[a] === player.simbolo &&
-        this.celulas[b] === player.simbolo &&
-        this.celulas[c] === player.simbolo
+        this.celulas[a] === player.simbolo.toString() &&
+        this.celulas[b] === player.simbolo.toString() &&
+        this.celulas[c] === player.simbolo.toString()
       );
     });
   }
@@ -50,7 +54,7 @@ class Board {
 class Game {
   board = new Board();
   players = [
-    new Player("Jogador", Simbolo.X),
+    new Player("Jogador 1", Simbolo.X),
     new Player("Jogador 2", Simbolo.O),
   ];
   currentPlayerIndex = 0;
@@ -61,16 +65,52 @@ class Game {
       const playerAtual = this.players[this.currentPlayerIndex];
       if (this.board.fazerJogada(playerAtual, index)) {
         if (this.board.verificaWin(playerAtual)) {
-          alert(`${playerAtual.nome} venceu`);
+          this.mensagem(`${playerAtual.nome} venceu`);
           this.gameOver = true;
         } else if (this.board.empatou()) {
-          alert(`empatou`);
+          this.mensagem(`empatou`);
           this.gameOver = true;
         } else {
           this.currentPlayerIndex = 1 - this.currentPlayerIndex;
-          alert(`${this.players[this.currentPlayerIndex].nome} é sua vez`);
+          this.mensagem(
+            `${this.players[this.currentPlayerIndex].nome} é sua vez`
+          );
         }
       }
     }
   }
+
+  mensagem(msg: string) {
+    const mensagemSpan = document.querySelector("span");
+    mensagemSpan!.textContent = msg;
+  }
+
+  resetar() {
+    this.board = new Board();
+    this.gameOver = false;
+    this.currentPlayerIndex = 0;
+    const cells = document.querySelectorAll(".cell");
+    document.querySelector("span")!.textContent = "";
+    cells.forEach((cell) => {
+      cell.textContent = "";
+    });
+  }
 }
+
+const jogo = new Game();
+
+const boardDiv = document.getElementById("board");
+const resetarButton = document.querySelector("button");
+
+boardDiv?.addEventListener("click", (evento) => {
+  const cell = (evento.target as HTMLDivElement) || null;
+  if (cell?.classList.contains("cell")) {
+    const index = cell.getAttribute("data-index");
+    jogo.fazerJogada(Number(index));
+    cell.textContent = jogo.players[jogo.currentPlayerIndex].simbolo.toString();
+  }
+});
+
+resetarButton?.addEventListener("click", (evento) => {
+  jogo.resetar();
+});
